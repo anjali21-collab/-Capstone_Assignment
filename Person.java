727fea4d0953ee8838b504ac3c9187ba9cedf88a
anjali21-collab <tomaranjali2108@gmail.com>
2025-12-01 22:46:09 +0530

    Create abstract Person class with basic attributes

---
+++ Person.java
@@ -0,0 +1,16 @@
+// Name: Anjali Tomar
+// Roll Number: 2401010080
+package model;
+public abstract class Person {
+    protected String name;
+    protected String email;
+
+    public Person() {}
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public abstract void displayInfo();
+}
